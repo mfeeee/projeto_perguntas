2e6e392cc0124125b0fe89731e6afc09820f8844
mfeeee <mfernandarocosta@gmail.com>
2025-08-05 10:13:25 -0300

    :sparkles: feat: criando widget Resposta

---
+++ lib/resposta.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Resposta extends StatelessWidget {
+
+  final String texto;
+
+  const Resposta({super.key, required this.texto});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+              onPressed: null,
+              child: Text(texto),
+    );
+  }
+}
