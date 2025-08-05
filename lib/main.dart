e2b50afa2fe99d00a824ff066ac4b41cce0cc71f
mfeeee <mfernandarocosta@gmail.com>
2025-08-05 11:14:13 -0300

    :recycle: refactor: usando tipo Map nas perguntas

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,29 @@ class _PerguntaAppState extends State<PerguntaApp> {
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
+    final List<Map<String, dynamic>> perguntas = [
+      {
+        'texto': 'O que significa PHP?',
+        'respostas': ['PHP: Hyperspeed Performance', 'PHP: Hyperformat Programming', 'PHP: Hypertext Preprocessor', 'PHP: Hyperlink Pages'],
+      },
+      {
+        'texto': 'Qual afirmação NÃO é verdadeira sobre PHP:',
+        'respostas': ['PHP é uma biblioteca de código Front End', 'PHP pode gerar conteúdo de página dinâmico', 'PHP é gratuito'],
+      },
+      {
+        'texto': 'Qual é a extensão de arquivo correta para arquivos PHP?',
+        'respostas': ['.php', '.ps', '.hpp'],
+      },
     ];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Perguntas'),
+          title: Text('Introdução PHP'),
         ),
         body: Column(
           children: [
-            Questao(texto: perguntas[_perguntaSelecionada]),
+            Questao(texto: perguntas[_perguntaSelecionada]['texto']),
             Resposta(texto: 'Resposta 1', onSelected: _responder),
             Resposta(texto: 'Resposta 2', onSelected: _responder),
             Resposta(texto: 'Resposta 3', onSelected: _responder),
